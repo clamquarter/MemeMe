30950f771d05d008343cc726e4037a4e88b5ca51
India Doria <indiadoria@gmail.com>
2020-07-06 18:28:59 -0400

    Update

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    //MARK: -Outlets
+    @IBOutlet var memeImage: UIImageView!
+    @IBOutlet var cameraButton: UIBarButtonItem!
+    @IBOutlet var albumButton: UIToolbar!
+    @IBOutlet var topText: UITextField!
+    @IBOutlet var bottomText: UITextField!
+    
+    //constant defining text attributes that have been applied to the meme text fields.
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "Impact", size: 36)!,
+        NSAttributedString.Key.strokeWidth: 3.0
+    ]
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        topText.defaultTextAttributes = memeTextAttributes
+        bottomText.defaultTextAttributes = memeTextAttributes
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
-
+    //MARK: -Actions
+    @IBAction func pickAnImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+        
+        }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        memeImage.image = image
+    
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
